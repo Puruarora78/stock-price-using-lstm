98395d3411a3197dcf4aa90ce48024cb2917a22b
Puru Arora <puruarora28@gmail.com>
2026-08-13 00:31:57 +0530

    comment out

--- main.py
+++ main.py
@@ -97,114 +97,113 @@ train_target = target.iloc[:train_data]
 val_target = target.iloc[train_data:val_data]
 test_target = target.iloc[val_data:]
 
-# TF_scaler = MinMaxScaler()
-# TT_scaler = MinMaxScaler() 
-# train_features_scaled = TF_scaler.fit_transform(train_features)
-# val_daatures_scaled = TF_scaler.transform(val_features)
-# test_features_scaled = TF_scaler.transform(test_features)
+TF_scaler = MinMaxScaler()
+TT_scaler = MinMaxScaler() 
+train_features_scaled = TF_scaler.fit_transform(train_features)
+val_daatures_scaled = TF_scaler.transform(val_features)
+test_features_scaled = TF_scaler.transform(test_features)
 
-# train_target_scaled = TT_scaler.fit_transform(train_target)
-# val_target_scaled = TT_scaler.transform(val_target)
-# test_target_scaled = TT_scaler.transform(test_target)
+train_target_scaled = TT_scaler.fit_transform(train_target)
+val_target_scaled = TT_scaler.transform(val_target)
+test_target_scaled = TT_scaler.transform(test_target)
 
-# # sliding window for training data x,y
-# x_train = []
-# y_train = []
-# for i in range(30,len(train_features_scaled)):
-#     x_train.append(train_features_scaled[i-30:i,:])
-#     y_train.append(train_target_scaled[i,0])
-# x_train = np.array(x_train)
-# y_train = np.array(y_train)
-
-
-# # sliding window for val
-# val_data_input = np.concatenate((train_features_scaled[-30:],val_daatures_scaled))
-# val_data_x = []
-# for i in range(30,len(val_data_input)):
-#     val_data_x.append(val_data_input[i-30:i,:])
-# val_data_x = np.array(val_data_x)
-# val_data_y = val_target_scaled
+# sliding window for training data x,y
+x_train = []
+y_train = []
+for i in range(30,len(train_features_scaled)):
+    x_train.append(train_features_scaled[i-30:i,:])
+    y_train.append(train_target_scaled[i,0])
+x_train = np.array(x_train)
+y_train = np.array(y_train)
 
 
-# model = keras.models.Sequential()
+# sliding window for val
+val_data_input = np.concatenate((train_features_scaled[-30:],val_daatures_scaled))
+val_data_x = []
+for i in range(30,len(val_data_input)):
+    val_data_x.append(val_data_input[i-30:i,:])
+val_data_x = np.array(val_data_x)
+val_data_y = val_target_scaled
 
 
-# # layers
-# model.add(keras.layers.LSTM(64, return_sequences= True , input_shape = (x_train.shape[1],x_train.shape[2])))
-# model.add(keras.layers.LSTM(64, return_sequences= False))
-# model.add(keras.layers.Dense(128, activation= "relu"))
-# model.add(keras.layers.Dropout(0.5))
-# model.add(keras.layers.Dense(1))
-
-# model.summary()
-# model.compile(optimizer= "adam",loss= "mae",metrics= [keras.metrics.RootMeanSquaredError()])
-
-# early_stop = keras.callbacks.EarlyStopping(monitor= "val_loss" ,patience= 3 ,restore_best_weights=True)
-
-# training = model.fit(x_train,y_train,epochs=20,batch_size=32,validation_data=(val_data_x,val_data_y),callbacks=[early_stop])
-
-# loss = training.history["loss"]
-# val_loss = training.history["val_loss"]
-# plt.figure(figsize =(12,8))
-# plt.plot(loss,label="loss",color="blue")
-# plt.plot(val_loss,label = "val loss", color = "green")
-# plt.legend()
-# plt.show()
-
-# # sliding window for current data it will predict whats the prediction from current data values of sma20,close
-# test_input = np.concatenate((train_features_scaled[-30:],test_features_scaled))
-# x_test = []
-# for i in range(30,len(test_input)):
-#     x_test.append(test_input[i-30:i,:])
-# x_test = np.array(x_test)
-# y_test = test_target_scaled
+model = keras.models.Sequential()
 
 
-# # x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
+# layers
+model.add(keras.layers.LSTM(64, return_sequences= True , input_shape = (x_train.shape[1],x_train.shape[2])))
+model.add(keras.layers.LSTM(64, return_sequences= False))
+model.add(keras.layers.Dense(128, activation= "relu"))
+model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(1))
 
-# future_price = model.predict(x_test)
-# future_price = TT_scaler.inverse_transform(future_price)
-# actual_price = TT_scaler.inverse_transform(y_test.reshape(-1,1))
+model.summary()
+model.compile(optimizer= "adam",loss= "mae",metrics= [keras.metrics.RootMeanSquaredError()])
+
+early_stop = keras.callbacks.EarlyStopping(monitor= "val_loss" ,patience= 3 ,restore_best_weights=True)
+
+training = model.fit(x_train,y_train,epochs=20,batch_size=32,validation_data=(val_data_x,val_data_y),callbacks=[early_stop])
+
+plt.plot(training.history["loss"], label="Training Loss")
+plt.plot(training.history["val_loss"], label="Validation Loss")
+plt.xlabel("Epoch")
+plt.ylabel("MAE")
+plt.title("Training vs Validation Loss")
+plt.legend()
+plt.show()
+
+# sliding window for current data it will predict whats the prediction from current data values of sma20,close
+test_input = np.concatenate((train_features_scaled[-30:],test_features_scaled))
+x_test = []
+for i in range(30,len(test_input)):
+    x_test.append(test_input[i-30:i,:])
+x_test = np.array(x_test)
+y_test = test_target_scaled
+
+
+# x_test = np.reshape(x_test,(x_test.shape[0],x_test.shape[1],1))
+
+future_price = model.predict(x_test)
+future_price = TT_scaler.inverse_transform(future_price)
+actual_price = TT_scaler.inverse_transform(y_test.reshape(-1,1))
 
 #   ----------------- naive baseline --------------------- 
 data_for_bl = np.concatenate((val_target["Close"].values[-1:],test_target["Close"].values[:-1]))
 test_values = test_target["Close"].values
 print(mean_absolute_error(data_for_bl,test_values))
 print(root_mean_squared_error(data_for_bl,test_values))
-# x_bl = []
-# y_bl = []
-# for i in range(1,len(data_for_bl)):
-#     x_bl.append(data_for_bl[i-1])
-#     y_bl.append(data_for_bl[i])
 
 
+# for aesthetic
+print("\n========== Model Evaluation ==========")
+mae = mean_absolute_error(future_price,actual_price)
+print(f"mae for LSTM is : {mae:.2f}")
 
+rmse = root_mean_squared_error(future_price,actual_price)
+print(f"rmse for LSTM is : {rmse:.2f}")
 
-# # for aesthetic
-# print("\n========== Model Evaluation ==========")
-# mae = mean_absolute_error(future_price,actual_price)
-# print(f"mae is : {mae:.2f}")
+mae = mean_absolute_error(data_for_bl,test_values)
+print(f"mae for Naive Baseline is : {mae:.2f}")
 
-# rmse = root_mean_squared_error(future_price,actual_price)
-# print(f"rmse is : {rmse:.2f}")
-# print("======================================")
+rmse = root_mean_squared_error(data_for_bl,test_values)
+print(f"rmse for Naive Baseline is : {rmse:.2f}")
+print("======================================")
 
-# # graph
-# plt.figure(figsize=(12,8))
-# plt.plot(train_features.index,train_features["Close"],label= "trained price", color= "red")
-# plt.plot(test_features.index,test_features["Close"],label= "desired actual price", color = "green")
-# plt.plot(val_features.index,val_features["Close"],label= "given val price", color = "purple")
-# plt.plot(test_features.index,future_price,label= "predicted price", color = "blue")
-# plt.title("price pridiction usind lstm")
-# plt.xlabel("date")
-# plt.ylabel("price")
-# plt.legend()
-# plt.savefig("images/pridicted_data.png", dpi=300, bbox_inches="tight")
-# plt.show()
+# graph
+plt.figure(figsize=(12,8))
+plt.plot(train_features.index,train_features["Close"],label= "trained price", color= "red")
+plt.plot(test_features.index,test_features["Close"],label= "desired actual price", color = "green")
+plt.plot(val_features.index,val_features["Close"],label= "given val price", color = "purple")
+plt.plot(test_features.index,future_price,label= "predicted price", color = "blue")
+plt.title("price pridiction usind lstm")
+plt.xlabel("date")
+plt.ylabel("price")
+plt.legend()
+plt.savefig("images/pridicted_data.png", dpi=300, bbox_inches="tight")
+plt.show()
 
-# plt.figure(figsize=(12,8))
-# plt.plot(test_features.index,test_features["Close"],label= "desired actual price", color = "green")
-# plt.plot(test_features.index,future_price,label= "predicted price", color = "blue")
-# plt.title("comparison")
-# plt.legend()
-# plt.show()
+plt.figure(figsize=(12,8))
+plt.plot(test_features.index,test_features["Close"],label= "desired actual price", color = "green")
+plt.plot(test_features.index,future_price,label= "predicted price", color = "blue")
+plt.title("comparison")
+plt.legend()
+plt.show()
